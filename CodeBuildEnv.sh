d430b98257c2117be8aef516ec7a1947cae09c3b
Jack Gallop <jackg@pageuppeople.com>
2018-07-27 09:12:29 +1000

    get branch script

---
+++ CodeBuildEnv.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export CI=true
+export CODEBUILD=true
+
+export CODEBUILD_GIT_BRANCH="$(git symbolic-ref HEAD --short 2>/dev/null)"
+if [ "$CODEBUILD_GIT_BRANCH" = "" ] ; then
+  CODEBUILD_GIT_BRANCH="$(git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }')";
+  export CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/};
+fi
+
+export CODEBUILD_PULL_REQUEST=false
+if [ "${CODEBUILD_GIT_BRANCH#pr-}" != "$CODEBUILD_GIT_BRANCH" ] ; then
+  export CODEBUILD_PULL_REQUEST=${CODEBUILD_GIT_BRANCH#pr-};
+fi
+
